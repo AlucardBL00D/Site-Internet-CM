1bef277ebe4f86472a2e53ef02bdc616ae81d8d5
Xavier <Xavier.Archambault.Pro@gmail.com>
2025-09-25 19:42:13 -0400

    Test avec index.html

--- Pages/templates/header.php
+++ Pages/templates/header.php
@@ -5,7 +5,7 @@
     <img src="image/Logo_CM_2_transparent_Gradient_2.png" alt="Logo Catherine Messier Designer & Finition INC." class="logo">
     <nav>
         <ul>
-            <li><a href="/index.php">Accueil</a></li>
+            <li><a href="/index.html">Accueil</a></li>
             <li><a href="/Pages/about.php">À propos</a></li>
             <li><a href="/Pages/services.php">Services</a></li>
             <li><a href="/Pages/portfolio.php">Portfolio</a></li>
